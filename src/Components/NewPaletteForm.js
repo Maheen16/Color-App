2db692b682fc47975a7b295ffbe2ddc2f2b0c959
Maheen <maheen66005@gmail.com>
2022-03-24 12:10:13 +0530

    Added ChromePicker and styled the drawer

--- src/Components/NewPaletteForm.js
+++ src/Components/NewPaletteForm.js
@@ -7,15 +7,28 @@ import MuiAppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
+import { Button } from "@mui/material";
+import { withStyles } from "@mui/styles";
 import MenuIcon from "@mui/icons-material/Menu";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import { ChromePicker } from "react-color";
 
-const drawerWidth = 240;
-export default class NewPaletteForm extends Component {
+const styles = () => ({
+  chromePicker: {
+    "& .chrome-picker": {
+      width: "310px !important",
+    },
+  },
+});
+
+const drawerWidth = 350;
+
+class NewPaletteForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
       open: true,
+      btnNewColor: "",
     };
   }
   handleDrawerOpen = () => {
@@ -24,9 +37,31 @@ export default class NewPaletteForm extends Component {
   handleDrawerClose = () => {
     this.setState({ open: false });
   };
+  colorHandleChange = (newColor) => {
+    console.log(newColor.hex);
+    // this.setState({ btnNewColor: newColor.hex });
+  };
   render() {
-    const { open } = this.state;
-
+    const { open, btnNewColor } = this.state;
+    const { classes } = this.props;
+    const Main = styled("main", {
+      shouldForwardProp: (prop) => prop !== "open",
+    })(({ theme, open }) => ({
+      flexGrow: 1,
+      padding: theme.spacing(3),
+      transition: theme.transitions.create("margin", {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      marginLeft: `-${drawerWidth}px`,
+      ...(open && {
+        transition: theme.transitions.create("margin", {
+          easing: theme.transitions.easing.easeOut,
+          duration: theme.transitions.duration.enteringScreen,
+        }),
+        marginLeft: 0,
+      }),
+    }));
     const DrawerHeader = styled("div")(({ theme }) => ({
       display: "flex",
       alignItems: "center",
@@ -71,7 +106,7 @@ export default class NewPaletteForm extends Component {
               <MenuIcon />
             </IconButton>
             <Typography variant="h6" noWrap component="div">
-              Persistent drawer
+              Create a Palette
             </Typography>
           </Toolbar>
         </AppBar>
@@ -93,8 +128,60 @@ export default class NewPaletteForm extends Component {
               <ChevronLeftIcon />
             </IconButton>
           </DrawerHeader>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              width: "100%",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Typography
+              variant="h5"
+              sx={{ letterSpacing: "5px", wordSpacing: "5px" }}
+            >
+              Design Your Palette
+            </Typography>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                width: "100%",
+                justifyContent: "center",
+                mb: 2,
+                mt: 2,
+              }}
+            >
+              <Button variant="contained" color="secondary" sx={{ mr: 2 }}>
+                Clear Palette
+              </Button>
+              <Button variant="contained" color="primary">
+                Random Color
+              </Button>
+            </Box>
+            <Box className={classes.chromePicker}>
+              <ChromePicker
+                color="purple"
+                onChangeComplete={(newColor) =>
+                  this.colorHandleChange(newColor)
+                }
+              />
+            </Box>
+            <Button
+              variant="contained"
+              color="primary"
+              sx={{ mt: 2, width: "90%", p: 2 }}
+            >
+              Add Palette
+            </Button>
+          </Box>
         </Drawer>
+        <Main open={open}>
+          <DrawerHeader />
+        </Main>
       </Box>
     );
   }
 }
+export default withStyles(styles)(NewPaletteForm);
